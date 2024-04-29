022ad66c11022a289855408389c02addac15c1c7
r3dotstone <jstone.import@gmail.com>
2024-04-29 07:17:37 -0400

    vision out workin

--- visionOut.py
+++ visionOut.py
@@ -3,17 +3,19 @@ import time
 
 # time = time.time() when measured
 cookies = [
-    {"pick": True, "posInt": 246, "time": 1714356330.463, "angle": 209.95, "posNow": 787},
-    {"pick": False, "posInt": 257, "time": 1714356330.877, "angle": 200.03, "posNow": 617},
-    {"pick": True, "posInt": 246, "time": 1714356331.249, "angle": 209.81, "posNow": 427},
-    {"pick": True, "posInt": 244, "time": 1714356331.669, "angle": 190.99, "posNow": 245}
+    {"pick": True, "posInt": 246, "time": 1714356330.463, "angle": 209.95, "posNow": 787, "id": 1},
+    {"pick": False, "posInt": 257, "time": 1714356330.877, "angle": 200.03, "posNow": 617, "id": 2},
+    {"pick": True, "posInt": 246, "time": 1714356331.249, "angle": 209.81, "posNow": 427, "id": 3},
+    {"pick": True, "posInt": 244, "time": 1714356331.669, "angle": 190.99, "posNow": 245, "id": 4}
 ]
 
 # Create the pandas DataFrame
 dfQ = pd.DataFrame(cookies)
+dfBox = pd.DataFrame(columns=dfQ.columns)
+dfBin = pd.DataFrame(columns=dfQ.columns)
 
 convVel = 450 # mm/s
-pickLocation = 2000 # mm from front edge of scanner
+posPick = 2000 # mm from front edge of scanner
 timeLast = time.time()
 printTimeLast = 0
 while True:
@@ -22,18 +24,24 @@ while True:
     timeLast = timeNow
     dfQ["posNow"] = dfQ["posNow"] + (convVel*dt)
 
-    dfPick = dfQ[(dfQ["pick"] == True) and (dfQ["posNow"] >= pickLocation)] 
-    if len(dfPick) == 1:
-        print("picking position", dfPick["posNow"])
+    dfPick = dfQ[(dfQ["pick"] == True) & (dfQ["posNow"] >= posPick)] 
+    if dfQ.empty: print("no more cookies :(")
+    elif (dfQ.at[dfQ.index[0],"pick"] == True) and (dfQ.at[dfQ.index[0],"posNow"] >= posPick):
+        print("picking id", dfQ.at[dfQ.index[0],"id"])
+        row_to_append = dfQ.loc[[dfQ.index[0]]]
+        dfBox = dfBox.append(row_to_append, ignore_index=True)
         dfQ = dfQ.drop(dfQ.index[0])
-    if len(dfPick) > 1:
+    elif (dfQ.at[dfQ.index[0],"pick"] == False) and (dfQ.at[dfQ.index[0],"posNow"] >= posPick):
+        print("binning id: ", dfQ.at[dfQ.index[0],"id"])
+        row_to_append = dfQ.loc[[dfQ.index[0]]]  # This keeps the row as a DataFrame
+        dfBin = dfBin.append(row_to_append, ignore_index=True)
+        dfQ = dfQ.drop(dfQ.index[0])
+    elif len(dfPick) > 1:
         print("cookie overload!")
     else: print("no cookies to pick :(")
-    # if dfQ["pick"] == True and dfQ["posNow"] >= 2000:
-
 
     if time.time() - printTimeLast >= 0.2:
         print("dt=",dt,"\n",dfQ)
         printTimeLast = timeNow
 
-    time.sleep(0.01)
+    time.sleep(0.2)
